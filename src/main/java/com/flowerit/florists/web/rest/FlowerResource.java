17cf6d9da034935b33a4f9d0c52c9a9bc3f65765
Kamil Szerlag <szerlag.kamil@gmail.com>
2022-01-08 22:47:44 +0100

    fix: Fix for create new flower

--- src/main/java/com/flowerit/florists/web/rest/FlowerResource.java
+++ src/main/java/com/flowerit/florists/web/rest/FlowerResource.java
@@ -64,7 +64,7 @@ public class FlowerResource {
         if (flowerDTO.getId() != null) {
             throw new BadRequestAlertException("A new flower cannot already have an ID", ENTITY_NAME, "idexists");
         }
-        FlowerDTO result = flowerService.update(flowerDTO);
+        FlowerDTO result = flowerService.save(flowerDTO);
         return ResponseEntity
             .created(new URI("/api/flowers/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId()))
@@ -98,7 +98,7 @@ public class FlowerResource {
             throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
         }
 
-        FlowerDTO result = flowerService.save(flowerDTO);
+        FlowerDTO result = flowerService.update(flowerDTO);
         return ResponseEntity
             .ok()
             .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, flowerDTO.getId()))
